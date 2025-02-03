2edea964915ed8f6d6e734eb8c865965f5f5fc36
sapanagupta25 <146055257+sapanagupta25@users.noreply.github.com>
2025-02-03 17:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,9 +9,29 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  //your code here  
+	 if (num <= 0 || num > 100000) return "Invalid Input";
 
+  const romanNumerals = [
+    ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
+    ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4],
+    ['I', 1]
+  ];
+
+  let result = "";
+  
+  for (let [symbol, value] of romanNumerals) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 }
+
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
